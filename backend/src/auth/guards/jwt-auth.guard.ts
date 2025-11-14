5047c0eec1d7a87a241a4a13efa7b3169c23256a
Redconhue <116825262+Redconhue@users.noreply.github.com>
2025-11-14 23:11:43 +0800

    Update jwt-auth.guard.ts

--- backend/src/auth/guards/jwt-auth.guard.ts
+++ backend/src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Type } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends (AuthGuard('jwt') as Type<any>) {}
