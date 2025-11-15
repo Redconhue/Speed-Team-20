7df09b861762bae78c50542a9987d50dc597cbe6
Redconhue <116825262+Redconhue@users.noreply.github.com>
2025-11-15 12:59:01 +0800

    Update auth.module.ts

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     PassportModule,
+    ConfigModule, // 添加 ConfigModule
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
