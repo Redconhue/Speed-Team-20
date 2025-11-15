feaa5601bb66413936005e7b019de1482e19fe8f
Redconhue <116825262+Redconhue@users.noreply.github.com>
2025-11-15 13:17:15 +0800

    Update auth.module.ts

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -10,8 +10,9 @@ import { JwtAuthGuard } from './guards/jwt-auth.guard';
 @Module({
   imports: [
     PassportModule,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
     JwtModule.register({
-      secret: (process.env.JWT_SECRET as string) || 'speed-team-secret-key',
+      secret: process.env.JWT_SECRET || 'speed-team-secret-key',
       signOptions: { expiresIn: '24h' },
     }),
     UsersModule,
