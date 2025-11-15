1f7eb66650f966fc8871b93de3aae4c894437b6a
Redconhue <116825262+Redconhue@users.noreply.github.com>
2025-11-15 13:38:03 +0800

    Update jwt-auth.guard.ts

--- backend/src/auth/guards/jwt-auth.guard.ts
+++ backend/src/auth/guards/jwt-auth.guard.ts
@@ -2,4 +2,5 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
 export class JwtAuthGuard extends AuthGuard('jwt') {}
